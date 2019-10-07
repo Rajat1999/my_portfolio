b8c9df3b39d2bb307836a78355c73ce49aba0829
Rajat <rajatgour95@gmail.com>
2019-10-07 23:03:54 +0530

    seprating css in different files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
 // alert("this site is under construction! :)");
+
+
+
+const url = 'https://official-joke-api.appspot.com/jokes/random';
+const setup = document.querySelector('.setup');
+const punchline = document.querySelector('.punchline');
+
+const res = fetch(url);
+res.then(response => response.json())
+.then(data => {
+    setup.textContent = data.setup 
+    punchline.textContent = data.punchline;
+})
+.catch(error => console.log(error))
+
+ 
