f64ebfe0223087f2aa78ad463d6f6ac79e01d487
Rajat <rajatgour95@gmail.com>
2019-11-04 03:33:22 +0530

    final code addition

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 
 
 
-const url = 'https://official-joke-api.appspot.com/jokes/random';
+const url = "https://official-joke-api.appspot.com/jokes/random";
 const setup = document.querySelector('.setup');
 const punchline = document.querySelector('.punchline');
 
@@ -12,6 +12,23 @@ res.then(response => response.json())
     setup.textContent = data.setup 
     punchline.textContent = data.punchline;
 })
-.catch(error => console.log(error))
+.catch(error => console.log(error));
 
- 
+//  <========================================<    GALLERY PAGE    >========================================>
+
+const img = document.querySelectorAll("#img");
+const blur = document.querySelector("#blur");
+const bg = document.querySelector("#bg");
+
+img.forEach((e) => {
+    e.addEventListener("click", () => {
+        blur.classList.toggle("display");
+        bg.classList.toggle("display");
+        // bg.style.display = "block";
+        bg.style.backgroundImage = `url("../images/typing_keyboard.png")`;
+        bg.style.backgroundPosition = "center";
+        bg.style.backgroundRepeat = "no-repeat";
+        bg.style.backgroundSize = "cover";
+        bg.style.transition = "5s";
+    });
+});
